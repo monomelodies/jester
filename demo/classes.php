0bc59b4be8d026e95dc5e29cfe1e8518065f747f
Marijn Ophorst <marijn@sensimedia.nl>
2016-10-02 15:48:31 +0200

    demo classes

---
+++ demo/classes.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Monomelodies\Tests;
+
+class Foo
+{
+    protected $foo = 'foo';
+
+    public function sayHi()
+    {
+        return $this->foo;
+    }
+}
+
+interface testInterface
+{
+}
+
+trait testTrait
+{
+}
+
